750df724365701e134cfefbf71fe5154614a04f3
Sandra <sandramannila@gmail.com>
2020-03-26 19:30:52 -0400

    Initial commit

---
+++ iOS_assign4/iOS_assign4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_assign4
+//
+//  Created by Sandra Mannila on 2020-03-26.
+//  Copyright © 2020 Sandra Mannila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
